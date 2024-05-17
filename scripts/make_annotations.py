7cf0581cf146712bfccbfaf85d279930b61f6920
Prabhanjan Prabhu <prabhanjanp500@gmail.com>
2024-05-17 16:59:12 +0530

    Models + training/data generation code

---
+++ scripts/make_annotations.py
@@ -0,0 +1,7 @@
+import os
+
+# copy all images into image_path
+image_path = "D:\\Interns\\Prabhanjan\\extended-keypoints-yolo\\AlphaPose\\examples\\demo"
+
+for f in os.listdir(image_path):
+    pass
